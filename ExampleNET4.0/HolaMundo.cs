5551e086d9fb5b106e68cefcecbb493541f10264
Xabier Montez <montcouronne@hotmail.com>
2025-02-15 10:26:49 -0600

    first commit

---
+++ ExampleNET4.0/HolaMundo.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+
+	static void Main(string[] args)
+	{
+		Console.Write("Teclea tu nombre:");
+		string nombrePersona = Console.ReadLine();
+
+		Console.WriteLine("!Hola " + nombrePersona);
+	}
+}
