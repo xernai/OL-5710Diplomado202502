a1d89d6cee5fed3271189dc2b050d8cc5bcfe832
Xabier Montez <montcouronne@hotmail.com>
2025-02-15 13:00:22 -0600

    second commit

---
+++ ExampleNET5.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExampleNET5._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Auladiser!");
+        }
+    }
+}
