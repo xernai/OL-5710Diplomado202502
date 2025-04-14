4a690dfd12b851f3d8e642fba85ac0be8d304b4d
Xabier Montez <montcouronne@hotmail.com>
2025-04-14 17:00:41 -0600

    Event Grid added

--- Azure/CosmosDB/Azure.CosmosDB.PeopleCRUD/Azure.CosmosDB.PeopleCRUD/Program.cs
+++ Azure/CosmosDB/Azure.CosmosDB.PeopleCRUD/Azure.CosmosDB.PeopleCRUD/Program.cs
@@ -22,7 +22,7 @@ namespace Azure.CosmosDB.PeopleCRUD
         static async Task Main(string[] args)
         {
             // Local
-            var connectionString = "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+            var connectionString = "";
 
             var client = new CosmosClientBuilder(connectionString)
                                .WithSerializerOptions(new CosmosSerializationOptions
