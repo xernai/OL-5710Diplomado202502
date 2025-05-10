591ea0aa479900015ac1239d75cc39142ea783b6
Xabier Montez <montcouronne@hotmail.com>
2025-05-10 11:56:15 -0600

    feat: add Unit Tests

---
+++ DataTypes/Unit Tests/CalculatorTests/CalculatorTests.cs
@@ -0,0 +1,89 @@
+using System;
+using Utilities;
+using Xunit;
+
+namespace CalculatorTests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Should_SumTwoNumbers_Success()
+        {
+            // AAA: Arrange, Act, Assert
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            var actualValue = calculator.Sum(2, 2);
+            var expectedValue = 4;
+
+            // Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void Should_RestTwoNumbers_Success()
+        {
+            // AAA: Arrange, Act, Assert
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            var actualValue = calculator.Rest(2, 2);
+            var expectedValue = 0;
+
+            // Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void Should_MultiplyTwoNumbers_Success()
+        {
+            // AAA: Arrange, Act, Assert
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            var actualValue = calculator.Multiply(2, 2);
+            var expectedValue = 4;
+
+            // Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void Should_DivideTwoNumbers_Success()
+        {
+            // AAA: Arrange, Act, Assert
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            var actualValue = calculator.Divide(2, 2);
+            var expectedValue = 1;
+
+            // Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void Should_DivideTwoNumbers_Failure()
+        {
+            // AAA: Arrange, Act, Assert
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            var actualValue = calculator.Divide(2, 0);
+            var expectedValue = 0;
+
+            // Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+        [Fact]
+        public void SumTwoNumbers_Failure()
+        {
+
+        }
+    }
+}
---
+++ DataTypes/Unit Tests/Utilities/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    // TDD: Test Driven Development
+    public class Calculator
+    {
+        public int Sum(int x, int y)
+        {
+            // TODO
+            var result = x + y;
+            return result;
+        }
+
+        public int Rest(int x, int y)
+        {
+            // TODO
+            var result = x - y;
+            return result;
+        }
+
+        public int Multiply(int x, int y)
+        {
+            // TODO
+            var result = x * y;
+            return result;
+        }
+
+        public int Divide(int x, int y)
+        {
+            // TODO
+            if (y == 0)
+                return 0;
+
+            var result = x / y;
+            return result;
+        }
+    }
+}
